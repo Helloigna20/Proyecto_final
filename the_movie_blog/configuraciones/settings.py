0ef5ae9c4a879d8c47ab53a8664d576958662790
Ignacio Ojeda <vvalentinojeda18@gmail.com>
2025-08-14 12:05:02 -0300

    Solucionando static

--- the_movie_blog/configuraciones/settings.py
+++ the_movie_blog/configuraciones/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-em@p4y(@%e-eu=n-c@%021@f^%)2rx0u0+pa=h4ks2!1&^a%%v
 DEBUG = True
 
 #ALLOWED_HOSTS = ['helloigna.pythonanywhere.com']
-ALLOWED_HOSTS = ['helloigna.pythonanywhere.com']
+ALLOWED_HOSTS = ['helloigna.pythonanywhere.com', '127.0.0.1']
 
 # Application definition
 
